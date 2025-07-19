dfdc38b88b79aec5efd0680fa9397a4c1a880617
marco-pastore-projects <marcopastore42@gmail.com>
2025-07-19 17:09:14 +0200

    Sviluppata prima logica per capire se la prima colonna contiene il giorno di riferimento oppure il time range

--- notebooks/data_import.py
+++ notebooks/data_import.py
@@ -1,9 +1,15 @@
 import pandas as pd
+from datetime import datetime
+import re
 
-df = pd.read_excel('C:/Users/mpast/Desktop/Tesi-Magistrale/A2A_8.xlsx')
+df = pd.read_excel('C:/Users/mpast/Desktop/Tesi-Magistrale/data/A2A/A2A_8.xlsx')
 
-print(df['Intervallo tempo'].str.contains('JAN').index)
+# tolgo la prima riga perchè è quella che contiene il sommario del time range annuale
+df = df.iloc[1:]
 
-# print(df.head())
+# creo una colonna di valori booleani che mi indicano se la prima colonna contiene una data 
+# oppure il time-range di riferimento
+df['Flag Giorno VS TimeRange'] = df['Intervallo tempo'].str.contains(r'[a-zA-Z]', na=False)
 
-
+pd.set_option('display.max_rows', 100)
+print(df.head(100))
